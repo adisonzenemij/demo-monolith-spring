2df381ddd5ca06250befb2deb3dee196e03d774f
Adison Jimenez <adisonzenemij@gmail.com>
2026-06-16 18:57:54 -0500

    feat: ebffa6b4-cc43-4e70-8c1b-f6d17bbb1283

---
+++ src/main/java/code/Bb1c16e9aa27Application.java
@@ -0,0 +1,13 @@
+package code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bb1c16e9aa27Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bb1c16e9aa27Application.class, args);
+	}
+
+}
---
+++ src/test/java/code/Bb1c16e9aa27ApplicationTests.java
@@ -0,0 +1,13 @@
+package code;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bb1c16e9aa27ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
